a7e1dfd517511e2a94392157360b57fc3732c026
nicknish09 <nnmarques97@gmail.com>
2019-05-29 19:12:50 -0300

    img orianna

--- src/scratch/Scratch.js
+++ src/scratch/Scratch.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Text, View } from 'react-native';
 import ScratchView from 'react-native-scratch';
+import orianna_img from '../assets/images/orianna.png';
 
 export default class Scratch extends Component {
 
@@ -32,7 +33,7 @@ export default class Scratch extends Component {
         threshold={30} // Report full scratch after 70 percentage, change as you see fit. Default is 50
         fadeOut={true} // Disable the fade out animation when scratch is done. Default is true
         placeholderColor="#aaace0" // Scratch color while image is loading (or while image not present)
-        imageUrl="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_9.jpg" // A url to your image (Optional)
+        imageUrl="https://i.imgur.com/6qNjIgz.jpg" // A url to your image (Optional)
         resourceName="akali" // An image resource name (without the extension like '.png/jpg etc') in the native bundle of the app (drawble for Android, Images.xcassets in iOS) (Optional)
         resizeMode="cover" // Resize the image to fit or fill the scratch view. Default is stretch
         onImageLoadFinished={this.onImageLoadFinished} // Event to indicate that the image has done loading
