d5ac9d6d9dc1d3312479f137727f96db0319bf56
nicknish09 <nnmarques97@gmail.com>
2019-05-29 10:42:07 -0300

    react native navigation no ios

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation";
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`Home`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+          name: "Home"
+        }
+    }
+  });
+});
